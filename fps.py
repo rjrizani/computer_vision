bc68b8d4fbd1645d08c44b8a6479ab580a801ecc
rjrizani <rjrizani@gmail.com>
2024-09-02 21:28:09 +0800

    fps

--- fps.py
+++ fps.py
@@ -30,4 +30,26 @@ def get_frames(video_part, output_folder):
             cv2.imwrite("{}/frame{:d}.jpg".format(output_folder, counter), frame)
             counter += 1
 
-get_frames(video_part, output_folder)
+#get_frames(video_part, output_folder)
+
+
+
+output_video_part = "slow_video.mp4"
+
+video_capture = cv2.VideoCapture(video_part)
+fps = video_capture.get(cv2.CAP_PROP_FPS)  # conversi data type from float to int
+print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+
+width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+output_video = cv2.VideoWriter(output_video_part, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+
+while True:
+    ret, frame = video_capture.read()
+    if not ret:
+        break
+    output_video.write(frame)
+
+video_capture.release()
+output_video.release()
